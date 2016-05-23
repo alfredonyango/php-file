790847839001a40beb8c6cec7972a10cdcd3c5ef
alfredonyango <alfredokoth27@gmail.com>
2016-05-23 20:19:45 +0300

    Create work.php

---
+++ work.php
@@ -0,0 +1,39 @@
+<html>
+
+<head><title>Hello Php</title>
+</head
+
+<body>
+
+
+
+<?php
+
+$first="The quick brown fox";
+$second="Jumped over the fence";
+$third=$first;
+$third.=$second;
+
+echo $third;
+?>
+<br/>
+Lowercase: <?php echo strtolower($third); ?><br />
+uppercase: <?php echo strtolower($third); ?><br />
+uppercase first: <?php echo ucfirst($third); ?><br />
+uppercase words: <?php echo ucwrds($third); ?><br />
+
+
+<br/>
+Length <?php echo strlen($third); ?><br/><br/>
+Trim: <?php eco "A" . trim("B C D") . "E"; ?><br/><br/>
+Find: <?php echo strstr ($third, "brown"); ?><br/><br/>
+Replace by string: <?php echo str_replace("quick" "super-fast", $third); ?>
+<br/><br/>
+
+Repeat: <?php echo str_repeat ($third, 2)?> <br/> <br/>
+Make Substring: <?php echo substr ($third, 5, 10)?> <br/> <br/>
+Find Position: <?php echo strpos ($third, "brown")?> <br/> <br/>
+find character: <?php echo strchr($third, "z")?> <br/> <br/>
+
+</body>
+</html>
